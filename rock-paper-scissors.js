091d7f20249ccafaa0fa32ee43ca128aecb1ecf8
adiferrer <adiferrer@gmail.com>
2022-05-03 16:14:37 +0800

    Update playRound() function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,29 @@
 function computerPlay() {
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * 3)];
 }
 
-function game(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase + playerSelection.slice(1).toLowerCase();
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
-        return `It's a tie! Both ${computerSelection}`;
-    } else if (playerSelection == 'Rock') {
-        if (computerSelection == 'Paper') {
-            return `You lose! Paper beats Rock`;
+        return ("It's a tie! Both players chose " + playerSelection);
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "You lose! Paper beats rock";
         } else {
-            return `You win! Rock beats Scissors`;
+            return "You win! Rock beats scissors";
         }
-    } else if ()
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return "You lose! Scissors beats paper";
+        } else {
+            return "You win! Paper beats rock";
+        }
+    } else {
+        if (computerSelection == "rock") {
+            return "You lose! Rock beats scissors";
+        } else {
+            return "You win! Scissors beats paper";
+        }
+    }
 }
